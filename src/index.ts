cffa978393c1ade71a847be2d408b58170ab3fa5
FoggDev <foggdev@gmail.com>
2023-10-09 16:00:56 -0600

    fix: Fixing SSR

--- src/index.ts
+++ src/index.ts
@@ -19,16 +19,20 @@ export function mergeConfig(defaults: object, userConfig: object): object {
 }
 
 export function loadUserConfig(configPath: string): object {
-  if (fs && !fs?.existsSync(configPath)) {
-    return {}
+  if (typeof window === 'undefined') {
+    if (!fs.existsSync(configPath)) {
+      return {}
+    }
+
+    const fileContents = fs.readFileSync(configPath, 'utf8')
+
+    try {
+      return JSON.parse(fileContents)
+    } catch (error) {
+      console.error(error)
+      return {}
+    }
   }
 
-  const fileContents = fs ? fs.readFileSync(configPath, 'utf8') : '{}'
-
-  try {
-    return JSON.parse(fileContents)
-  } catch (error) {
-    console.error(error)
-    return {}
-  }
+  return {}
 }
